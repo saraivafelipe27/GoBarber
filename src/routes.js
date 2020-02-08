adb41381b0299e5b0bcf2ef74b82cc8295b14b29
Felipe Oliveira Saraiva <saraivafelipe27@gmail.com>
2020-02-07 22:58:28 -0300

    Chore: Adicionado e configurado Eslint, Prettier & EditorConfig

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: 'Hello rocketseat' });
-})
+routes.get('/', (req, res) => res.json({ message: 'Hello rocketseat' }));
 
 export default routes;

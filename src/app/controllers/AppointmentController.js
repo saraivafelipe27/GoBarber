bedccf38f14699edaba7f60222336312be50b863
Felipe Oliveira Saraiva <saraivafelipe27@gmail.com>
2020-02-19 17:52:29 -0300

    Feature: validação de agendamento criada utilizado date-fns

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,6 +1,7 @@
 import * as Yup from 'yup';
-import Appointment from '../models/Appointment';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
+import Appointment from '../models/Appointment';
 
 class AppointmentController {
   async store(req, res) {
@@ -29,10 +30,30 @@ class AppointmentController {
         .json({ error: 'You can only create appointments with providers' });
     }
 
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Appointment date is not available' });
+    }
+
     const appointment = await Appointment.create({
       user_id: req.userID,
       provider_id,
-      date,
+      date: hourStart,
     });
 
     return res.json(appointment);

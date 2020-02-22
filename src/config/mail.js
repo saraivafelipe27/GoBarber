e95050d8112b1a5415df530a110609afc4630808
Felipe Oliveira Saraiva <saraivafelipe27@gmail.com>
2020-02-22 12:42:17 -0300

    Feature: Configurando nodemailer, utilizando mailTrap para teste

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'eab1f27307f55c',
+    pass: '281f38e72414d6',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
